911177f3438feb006c4d441837c29aa9ce90db09
Anatol V <malfenius@gmail.com>
2018-07-08 18:13:09 +0300

    Разбор строк. Решение 80 задачи.

---
+++ src/main/java/Ex80.java
@@ -0,0 +1,186 @@
+package main.java;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Ex80 {
+    private static final Scanner in = new Scanner(System.in);
+    private static final PrintWriter out = new PrintWriter(System.out);
+
+    private static final int NUMBER_OF_ARGUMENTS = 3;
+    private static final int MAX_NUMBER = 30_000;
+    private static final int MAX_STRING = 101;
+    private static final String ERROR = "ERROR";
+    private static final String YES = "YES";
+    private static final String NO = "NO";
+
+    enum Sign {
+        MINUS('-'), PLUS('+'), MULTIPLY('*'), DIVIDE('/');
+
+        final char _symbol;
+        Sign(char symbol){
+            this._symbol = symbol;
+        }
+
+        static boolean contains(char operationSymbol) {
+            Sign[] signs = values();
+            for (int i = 0; i < signs.length; i++) {
+                if(signs[i]._symbol == operationSymbol){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    private static boolean isNumber(char symbol) {
+        return symbol >= '0' && symbol <= '9';
+    }
+
+    private static boolean isOperation(char operationSymbol) {
+        return Sign.contains(operationSymbol);
+    }
+
+    public static String parse(String expression) {
+        float firstArg = 0;
+        float secondArg;
+        float thirdArg = 0;
+        float resultExpression = 0;
+
+        try {
+            if(!(expression.length() > MAX_STRING)){
+                int indexString = 0;
+                for (int i = 0; i < NUMBER_OF_ARGUMENTS; i++) {
+                    switch (i){
+                        // первое число (со знаком минус)
+                        case 0: {
+                            // начинается со знака минус
+                            boolean isMinus = false;
+                            if (expression.charAt(indexString) == '-') {
+                                isMinus = true;
+                                indexString++;
+                            }
+                            // является числом
+                            if (!isNumber(expression.charAt(indexString))) {
+                                return ERROR;
+                            }
+
+                            StringBuilder sb = isMinus
+                                    ? new StringBuilder("-")
+                                    : new StringBuilder();
+
+                            for (; indexString < expression.length() && isNumber(expression.charAt(indexString)); indexString++) {
+                                sb.append(expression.charAt(indexString));
+                            }
+
+                            if (sb.length() == 0) {
+                                return ERROR;
+                            }
+                            int number = Integer.parseInt(sb.toString());
+                            if (Math.abs(number) > MAX_NUMBER) {
+                                return ERROR;
+                            }
+                            firstArg = number;
+                            break;
+                        }
+                        case 1: {
+                            char sign = expression.charAt(indexString++);
+                            if (!isOperation(sign)) {
+                                return ERROR;
+                            }
+
+                            boolean isMinus = false;
+                            char symbol = expression.charAt(indexString);
+                            while (isOperation(symbol)) {
+                                symbol = expression.charAt(indexString++);
+                                isMinus ^= (symbol == Sign.MINUS._symbol);
+                            }
+
+                            StringBuilder sb = isMinus
+                                    ? new StringBuilder("-")
+                                    : new StringBuilder();
+
+                            if(isMinus){ indexString--; }
+                            for (; indexString < expression.length() && isNumber(expression.charAt(indexString)); indexString++) {
+                                sb.append(expression.charAt(indexString));
+                            }
+
+                            if (sb.length() == 0) {
+                                return ERROR;
+                            }
+
+                            int number = Integer.parseInt(sb.toString());
+                            if (Math.abs(number) > MAX_NUMBER) {
+                                return ERROR;
+                            }
+
+                            secondArg = number;
+
+                            switch (sign) {
+                                case '+':
+                                    resultExpression = firstArg + secondArg;
+                                    break;
+                                case '-':
+                                    resultExpression = firstArg - secondArg;
+                                    break;
+                                case '*':
+                                    resultExpression = firstArg * secondArg;
+                                    break;
+                                case '/':
+                                    resultExpression = firstArg / secondArg;
+                                    break;
+                            }
+                            break;
+                        }
+                        case 2: {
+                            if (expression.charAt(indexString++) != '=') {
+                                return ERROR;
+                            }
+
+                            // начинается со знака минус
+                            boolean isMinus = false;
+                            if (expression.charAt(indexString) == '-') {
+                                isMinus = true;
+                                indexString++;
+                            }
+
+                            if (!isNumber(expression.charAt(indexString))) {
+                                return ERROR;
+                            }
+
+                            StringBuilder sb = isMinus
+                                    ? new StringBuilder("-")
+                                    : new StringBuilder();
+
+                            for (; indexString < expression.length() && isNumber(expression.charAt(indexString)); indexString++) {
+                                sb.append(expression.charAt(indexString));
+                            }
+
+                            if (sb.length() == 0 || indexString != expression.length()) {
+                                return ERROR;
+                            }
+                            int number = Integer.parseInt(sb.toString());
+                            if (Math.abs(number) > MAX_NUMBER) {
+                                return ERROR;
+                            }
+                            thirdArg = number;
+                            break;
+                        }
+                        default:
+                            return ERROR;
+                    }
+                }
+            }
+            return resultExpression == thirdArg
+                    ? YES : NO;
+        } catch (StringIndexOutOfBoundsException exc) {
+            return ERROR;
+        }
+    }
+
+    public static void main(String[] args) {
+        String expression = in.nextLine();
+        out.print(parse(expression));
+        out.flush();
+    }
+}
---
+++ src/test/java/Ex80Test.java
@@ -0,0 +1,78 @@
+package test.java;
+
+import main.java.Ex80;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Ex80Test {
+    private static final String YES = "YES";
+    private static final String NO = "NO";
+    private static final String ERROR = "ERROR";
+
+    @Test
+    public void testEqualsTrue(){
+        // Число | операция (более 1 раза) | число | знак равенства (более одного раза) | число
+        // (один знак минус) Число | операция (более 1 раза) | число | знак равенства (более одного раза) | число
+        Assert.assertEquals(YES, Ex80.parse("2+3=5"));
+        Assert.assertEquals(YES, Ex80.parse("3*7=21"));
+        Assert.assertEquals(YES, Ex80.parse("2-1=1"));
+        Assert.assertEquals(YES, Ex80.parse("4/2=2"));
+
+        Assert.assertEquals(YES, Ex80.parse("-2+-2=-4"));
+        Assert.assertEquals(YES, Ex80.parse("-2*-2=4"));
+        Assert.assertEquals(YES, Ex80.parse("-2/-2=1"));
+        Assert.assertEquals(YES, Ex80.parse("-2--2=0"));
+
+        Assert.assertEquals(YES, Ex80.parse("-0+1=1"));
+        Assert.assertEquals(YES, Ex80.parse("-01+1=0"));
+        Assert.assertEquals(YES, Ex80.parse("-01*1=-1"));
+        Assert.assertEquals(YES, Ex80.parse("-01/1=-1"));
+        Assert.assertEquals(YES, Ex80.parse("-01-1=-2"));
+
+        Assert.assertEquals(YES, Ex80.parse("-5+-7=-12"));
+        Assert.assertEquals(YES, Ex80.parse("04+04=08"));
+        Assert.assertEquals(YES, Ex80.parse("-3-2=-5"));
+        Assert.assertEquals(YES, Ex80.parse("3*-5=-15"));
+        Assert.assertEquals(YES, Ex80.parse("4*-0=0"));
+        Assert.assertEquals(YES, Ex80.parse("-0*2=0"));
+        Assert.assertEquals(YES, Ex80.parse("-12--7=-5"));
+        Assert.assertEquals(YES, Ex80.parse("2*-5=-10"));
+    }
+
+    @Test
+    public void testEqualsFalse(){
+        Assert.assertEquals(NO, Ex80.parse("-5--7=-12"));
+        Assert.assertEquals(NO, Ex80.parse("6/5=1"));
+        Assert.assertEquals(NO, Ex80.parse("123+-3=126"));
+        Assert.assertEquals(NO, Ex80.parse("123/0=1"));
+        Assert.assertEquals(NO, Ex80.parse("0/0=0"));
+        Assert.assertEquals(NO, Ex80.parse("-4875*-998=-3742"));
+    }
+
+    @Test
+    public void testEqualsError(){
+        Assert.assertEquals(ERROR, Ex80.parse("+2+3=5"));
+        Assert.assertEquals(ERROR, Ex80.parse("*2+3=5"));
+        Assert.assertEquals(ERROR, Ex80.parse("/2+3=5"));
+        Assert.assertEquals(ERROR, Ex80.parse("-/2+3=5"));
+        Assert.assertEquals(ERROR, Ex80.parse("2 + 3 = 5"));
+        Assert.assertEquals(ERROR, Ex80.parse("4+4 = 8"));
+        Assert.assertEquals(ERROR, Ex80.parse("4+4=8.0"));
+        Assert.assertEquals(ERROR, Ex80.parse("-1-1=-1-1"));
+        Assert.assertEquals(ERROR, Ex80.parse("132+3="));
+        Assert.assertEquals(ERROR, Ex80.parse("1-1-1=2"));
+        Assert.assertEquals(ERROR, Ex80.parse("+3f+3+sg=3"));
+        Assert.assertEquals(ERROR, Ex80.parse("3/2+3/2=3"));
+        Assert.assertEquals(ERROR, Ex80.parse("2=3-1"));
+        Assert.assertEquals(ERROR, Ex80.parse("1+3=2+2"));
+        Assert.assertEquals(ERROR, Ex80.parse("1+1+1+1+1+1+1+1+1+1=10"));
+        Assert.assertEquals(ERROR, Ex80.parse("(2*2)=4"));
+        Assert.assertEquals(ERROR, Ex80.parse("10^2+10^2=200"));
+        Assert.assertEquals(ERROR, Ex80.parse("2+3*7=23"));
+        Assert.assertEquals(ERROR, Ex80.parse("2*=3"));
+        Assert.assertEquals(ERROR, Ex80.parse("173"));
+        Assert.assertEquals(ERROR, Ex80.parse("2+2=a"));
+        Assert.assertEquals(ERROR, Ex80.parse("--2-2=0"));
+        Assert.assertEquals(ERROR, Ex80.parse("two plus three is five"));
+    }
+}
