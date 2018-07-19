e9dd71b49797e7abdc97a65b97779a4473d13812
Anatol V <malfenius@gmail.com>
2018-07-19 22:37:28 +0300

    Разбор строк. Задача 164. Первая попытка

--- src/main/java/strings/Ex164.java
+++ src/main/java/strings/Ex164.java
@@ -1,53 +1,88 @@
 package main.java.strings;
 
 import java.io.PrintWriter;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Scanner;
+
+class Ticket {
+    private static final int BOUND_END = 2;
+    private static final int SKIP_VALUE = 0;
+    private final String ticketNumber;
+
+    public Ticket(String number) {
+        ticketNumber = number;
+    }
+
+    private Deque<Integer> prepareTicketFormat() {
+        Deque<Integer> ticket = new LinkedList<>();
+        for (int i = 0; i < ticketNumber.length(); i++) {
+            int value = Character.getNumericValue(ticketNumber.charAt(i));
+            if(value != SKIP_VALUE){
+                ticket.addFirst(value);
+            }
+        }
+        return ticket;
+    }
+
+    // 11111 1111 9 - YES
+    // 9 1111 11111 - YES
+    private boolean runAnalyzeTicket(Deque<Integer> deque) {
+        if(deque.size() < 2){
+            return false;
+        }
+
+        int left = 0;
+        int right = 0;
+        while(deque.size() != 2){
+            int l = ((LinkedList<Integer>) deque).get(deque.size() - 1) + ((LinkedList<Integer>) deque).get(deque.size() - 2);
+            if(left <= right && l != 10){
+                left = deque.pollFirst() + deque.pollFirst();
+                if(left == 10){
+                    deque.addFirst(1);
+                } else if(left > 10){
+                    deque.addFirst(left % 10);
+                    deque.addFirst(left / 10);
+                } else {
+                    deque.addFirst(left);
+                }
+            } else {
+                right = deque.pollLast() + deque.pollLast();
+                if(right == 10){
+                    deque.addLast(1);
+                } else if(right > 10){
+                    deque.addLast(right / 10);
+                    deque.addLast(right % 10);
+                } else {
+                    deque.addLast(right);
+                }
+            }
+        }
+        left = deque.pollFirst();
+        right = deque.pollLast();
+
+        return left == right;
+    }
+
+    private boolean handleTicket() {
+        Deque<Integer> deque = prepareTicketFormat();
+        return runAnalyzeTicket(deque);
+    }
+
+    public boolean isHappy(){
+        return handleTicket();
+    }
+}
 
 public class Ex164 {
     private static final Scanner in = new Scanner(System.in);
     private static final PrintWriter out = new PrintWriter(System.out);
-
-    private static final String ZERO = "0";
-    private static final String EMPTY_PATTERN = "";
-
     private static final String YES = "YES";
     private static final String NO = "NO";
 
-    private static int[] prepareNumber(String s){
-        s = s.replaceAll(ZERO, EMPTY_PATTERN);
-        int[] numbers = new int[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            numbers[i] = Character.getNumericValue(s.charAt(i));
-        }
-        return numbers;
-    }
-
-    private static boolean isHappyTicket(String input) {
-        int left = 0, right = 0;
-        int[] numbers = prepareNumber(input);
-        int N = numbers.length;
-        for (int i = 0; i < N; i++) {
-            if(left > 9 || right > 9){
-                String buffer = String.valueOf(left);
-                buffer += input.substring(i, N-i);
-                buffer += String.valueOf(right);
-                return isHappyTicket(buffer);
-            }
-            if(left == 0) {
-                left += numbers[i];
-                right += numbers[N - i - 1];
-                continue;
-            }
-            if(left == right && i >= N / 2){
-                return true;
-            }
-            if(left > right && left + numbers[i] < 10){
-                right += numbers[N-i];
-            } else if(left < right && right + numbers[N-i] < 10) {
-                left += numbers[i];
-            }
-        }
-        return false;
+    public static boolean isHappyTicket(String number){
+        Ticket ticket = new Ticket(number);
+        return ticket.isHappy();
     }
 
     public static void main(String[] args) {
--- src/test/java/strings/Ex164Test.java
+++ src/test/java/strings/Ex164Test.java
@@ -3,25 +3,20 @@ package test.java.strings;
 import main.java.strings.Ex164;
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+
 
 public class Ex164Test {
 
-    private boolean callMethodIsHappyTicket(String inStr) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Method method = Ex164.class.getDeclaredMethod("isHappyTicket", String.class);
-        method.setAccessible(true);
-        return (boolean) method.invoke(Ex164.class, inStr);
-    }
-
     @Test
     public void testIsHappyTicket() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Assert.assertTrue(callMethodIsHappyTicket("0015420"));
-        Assert.assertTrue(callMethodIsHappyTicket("15420"));
-        Assert.assertTrue(callMethodIsHappyTicket("162510021"));
-        Assert.assertTrue(callMethodIsHappyTicket("911"));
+        Assert.assertTrue(Ex164.isHappyTicket("0015420"));
+        Assert.assertTrue(Ex164.isHappyTicket("15420"));
+        Assert.assertTrue(Ex164.isHappyTicket("162510021"));
+        Assert.assertTrue(Ex164.isHappyTicket("911"));
+        Assert.assertTrue(Ex164.isHappyTicket("119"));
+        Assert.assertTrue(Ex164.isHappyTicket("11111111111"));
 
-        Assert.assertFalse(callMethodIsHappyTicket("00100"));
+//        Assert.assertFalse(callMethodIsHappyTicket("00100"));
     }
 }
