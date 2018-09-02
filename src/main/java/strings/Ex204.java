75f87b7293e49e3edd3a6925e15a06451d03673b
Anatol V <malfenius@gmail.com>
2018-09-02 23:06:31 +0300

    Разбор строк. Задача 204. Первая попытка

---
+++ src/main/java/strings/Ex204.java
@@ -0,0 +1,54 @@
+package main.java.strings;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Ex204 {
+    private static final int MAX_LENGTH = 50_000;
+
+    private static boolean isValid(String stroka){
+        return stroka.length() <= MAX_LENGTH
+                && stroka.matches("\\w+");
+    }
+
+    private static int[] prefix_function(String stroka){
+        int n = stroka.length();
+        int[] pi = new int[n];
+        for (int i = 1; i < n; ++i)
+        {
+            int j = pi[i-1];
+            while ((j > 0) && (stroka.charAt(i) != stroka.charAt(j)))
+                j = pi[j-1];
+
+            if (stroka.charAt(i) == stroka.charAt(j))
+                ++j;
+
+            pi[i] = j;
+        }
+
+        return pi;
+    }
+
+    private static int getMinimalSubString(String stroka){
+        int[] weight = prefix_function(stroka);
+
+        int count = 0;
+        while(weight[count] == 0){
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        String stroka = in.next();
+        if(isValid(stroka)){
+            out.print(getMinimalSubString(stroka));
+        }
+
+        out.flush();
+    }
+}
---
+++ src/test/java/strings/Ex204Test.java
@@ -0,0 +1,26 @@
+package test.java.strings;
+
+import main.java.strings.Ex204;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Ex204Test {
+    private Method method;
+
+    public Ex204Test() throws NoSuchMethodException {
+        method = Ex204.class.getDeclaredMethod("getMinimalSubString", String.class);
+        method.setAccessible(true);
+    }
+
+    @Test
+    public void testSuccess() throws InvocationTargetException, IllegalAccessException {
+        int result = (int) method.invoke(Ex204.class, "aaaaaaaaaaa");
+        Assert.assertEquals(1, result);
+
+        result = (int) method.invoke(Ex204.class, "abababa");
+        Assert.assertEquals(2, result);
+    }
+}
