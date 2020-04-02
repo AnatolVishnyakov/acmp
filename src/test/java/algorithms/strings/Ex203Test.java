408523baf962726193eef505df6fe62d1ff77c12
Anatol V <malfenius@gmail.com>
2020-04-02 11:35:12 +0300

    Переделал на параметризированные тесты

--- src/test/java/algorithms/strings/Ex203Test.java
+++ src/test/java/algorithms/strings/Ex203Test.java
@@ -1,71 +1,64 @@
-package test.java.strings;
+package algorithms.strings;
 
-import main.java.strings.Ex203;
-import org.junit.Test;
-import org.junit.Assert;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 public class Ex203Test {
-    private Method method;
+    private Ex203.ShiftIdentifier shiftIdentifier = new Ex203.ShiftIdentifier();
 
-    public Ex203Test() throws NoSuchMethodException {
-        method = Ex203.class.getDeclaredMethod("check", String.class, String.class);
-        method.setAccessible(true);
+    public static Iterable<Object[]> testDataSuccess() {
+        return Arrays.asList(new Object[][]{
+                {"a", "a", 0},
+                {"BBBBaAAAAAAA", "BBBBaAAAAAAA", 0},
+                {"abbebeeaokeb", "abbebeeaokeb", 0},
+                {"abcde", "deabc", 2},
+                {"aaaaabbbbb", "bbbbbaaaaa", 5},
+                {"aaaaaaaaaab", "aaaaabaaaaa", 6},
+                {"abbebeeaokeb", "bbebeeaokeba", 11},
+                {"abbebeeaokeb", "babbebeeaoke", 1},
+                {"AaaA", "aAAa", 2},
+                {"abababab", "babababa", 7}
+        });
     }
 
-    @Test
-    public void testSuccess() throws InvocationTargetException, IllegalAccessException {
-        int result = (int) method.invoke(Ex203.class, "abcde", "deabc");
-        Assert.assertEquals(2, result);
-
-        result = (int) method.invoke(Ex203.class, "a", "a");
-        Assert.assertEquals(0, result);
-
-        result = (int) method.invoke(Ex203.class, "aaaaabbbbb", "bbbbbaaaaa");
-        Assert.assertEquals(5, result);
-
-        result = (int) method.invoke(Ex203.class, "BBBBaAAAAAAA", "BBBBaAAAAAAA");
-        Assert.assertEquals(0, result);
-
-        result = (int) method.invoke(Ex203.class, "aaaaaaaaaab", "aaaaabaaaaa");
-        Assert.assertEquals(6, result);
-
-        result = (int) method.invoke(Ex203.class, "abbebeeaokeb", "abbebeeaokeb");
-        Assert.assertEquals(0, result);
-
-        result = (int) method.invoke(Ex203.class, "abbebeeaokeb", "bbebeeaokeba");
-        Assert.assertEquals(11, result);
-
-        result = (int) method.invoke(Ex203.class, "abbebeeaokeb", "babbebeeaoke");
-        Assert.assertEquals(1, result);
-
-        result = (int) method.invoke(Ex203.class, "AaaA", "aAAa");
-        Assert.assertEquals(2, result);
-
-        result = (int) method.invoke(Ex203.class, "abababab", "babababa");
-        Assert.assertEquals(1, result);
+    @ParameterizedTest(name = "[{index}]: {0} -> {1} = {2}")
+    @MethodSource("testDataSuccess")
+    public void testSuccess(String pattern, String text, int expected) {
+        assertEquals(expected, shiftIdentifier.shelfRight(pattern, text));
     }
 
-    @Test
-    public void testNegative() throws InvocationTargetException, IllegalAccessException {
-        int result = (int) method.invoke(Ex203.class, "a", "b");
-        Assert.assertEquals(-1, result);
+    public static Iterable<Object[]> testDataFailed() {
+        return Arrays.asList(new Object[][]{
+                {"a", "b", -1},
+                {"abc", "bcd", -1},
+                {"aa", "ab", -1},
+                {"zxcvzxcvza", "zxcvzxcvaz", -1}
+        });
+    }
 
-        result = (int) method.invoke(Ex203.class, "abc", "bcd");
-        Assert.assertEquals(-1, result);
+    @ParameterizedTest(name = "[{index}]: {0} -> {1} = {2}")
+    @MethodSource("testDataFailed")
+    public void testNegative(String pattern, String text, int expected) {
+        assertEquals(expected, shiftIdentifier.shelfRight(pattern, text));
+    }
 
-        result = (int) method.invoke(Ex203.class, "aa", "ab");
-        Assert.assertEquals(-1, result);
+    public static Iterable<Object[]> testDataIsValid() {
+        return Arrays.asList(new Object[][]{
+                {"", "", false},
+                {"aa", "abc", false},
+                {"abc", "bcd", true},
+                {"", "abc", false},
+                {"abc123", "abc123", false}
+        });
+    }
 
-        result = (int) method.invoke(Ex203.class, "123123abc", "123123abc");
-        Assert.assertEquals(-1, result);
-
-        result = (int) method.invoke(Ex203.class, "zxcvzxcvza", "zxcvzxcvaz");
-        Assert.assertEquals(-1, result);
-
-        result = (int) method.invoke(Ex203.class, "", "");
-        Assert.assertEquals(-1, result);
+    @ParameterizedTest(name = "[{index}]: {0} -> {1} = {2}")
+    @MethodSource("testDataIsValid")
+    public void isValid(String pattern, String text, boolean expected) {
+        assertEquals(expected, shiftIdentifier.isValid(text, pattern));
     }
 }
