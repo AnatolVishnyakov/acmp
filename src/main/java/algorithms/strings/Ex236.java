f1324362b9d92c34219da5b2bcc588610358770d
Anatol V <malfenius@gmail.com>
2020-04-06 21:57:24 +0300

    Заготовка для задачи Ex236

---
+++ src/main/java/algorithms/strings/Ex236.java
@@ -0,0 +1,16 @@
+package algorithms.strings;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Ex236 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        String polynomial = in.nextLine();
+        int x = in.nextInt();
+
+        out.flush();
+    }
+}
