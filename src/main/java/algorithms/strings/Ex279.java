9ad8642393260bf5df3d199bbb5f3fabfa1d8040
Anatol V. <malfenius@gmail.com>
2025-05-03 23:47:03 +0300

    Ex279. Скобочная последовательность

--- src/main/java/algorithms/strings/Ex279.java
+++ src/main/java/algorithms/strings/Ex279.java
@@ -1,52 +1,55 @@
 package algorithms.strings;
 
+import java.io.PrintWriter;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Ex279 {
 
-    LinkedList<Character> queue = new LinkedList<Character>();
+    private static final int MAX_LENGTH = 100_000;
+    private static final LinkedList<Character> queue = new LinkedList<>();
 
-    public boolean check(String s) {
-        int magicAttempts = 1;
+    public static int check(String s) {
+        int magicAttempts = 0;
+        if (s == null || s.isEmpty()) {
+            return magicAttempts;
+        }
 
-        if (s.isEmpty()) return true;
         for (Character ch : s.toCharArray()) {
             switch (ch) {
-                case '{':
                 case '[':
                 case '(':
                     queue.add(ch);
                     continue;
-                case '}':
-                    if (queue.isEmpty()) return false;
-                    if (queue.peekLast() == '{' || magicAttempts == 1) {
-                        queue.pollLast();
-                        magicAttempts--;
-                    }
-                    continue;
                 case ']':
-                    if (queue.isEmpty()) return false;
-                    if (queue.peekLast() == '[' || magicAttempts == 1) {
-                        queue.pollLast();
-                        magicAttempts--;
+                    if (queue.isEmpty()) return -1;
+                    if (queue.pollLast() != '[') {
+                        magicAttempts++;
                     }
                     continue;
                 case ')': {
-                    if (queue.isEmpty()) return false;
-                    if (queue.peekLast() == '(' || magicAttempts == 1) {
-                        queue.pollLast();
-                        magicAttempts--;
+                    if (queue.isEmpty()) return -1;
+                    if (queue.pollLast() != '(') {
+                        magicAttempts++;
                     }
                     continue;
                 }
                 default:
-                    return false;
+                    return -1;
             }
         }
-        return queue.isEmpty();
+        return magicAttempts;
     }
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
 
+        String s = in.next();
+        if (s.length() <= MAX_LENGTH) {
+            out.print(check(s));
+        }
+
+        out.flush();
     }
 }
--- src/test/java/algorithms/strings/Ex279Test.java
+++ src/test/java/algorithms/strings/Ex279Test.java
@@ -2,6 +2,7 @@ package algorithms.strings;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class Ex279Test {
@@ -10,30 +11,51 @@ class Ex279Test {
     @ParameterizedTest
     @ValueSource(strings = {
             "",
-            "{}",
             "[]",
             "()",
-            "[]{}()",
-            "[({})]",
+            "[]()",
+            "[()]",
             "[][][][][][]",
             "()()()()()()",
-            "{}{}{}{}{}{}",
-            "{)",
-            "[}",
             "(]",
-            "([})"
+            "[)",
+            "([)]"
     })
     void correct(String input) {
-        Assertions.assertTrue(ex279.check(input));
+//        Assertions.assertTrue(ex279.check(input));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {
             "{))",
-            "[}}",
             "(]]"
     })
     void incorrect(String input) {
-        Assertions.assertFalse(ex279.check(input));
+//        Assertions.assertFalse(ex279.check(input));
     }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "(())[]:0",
+                    "[(]):2",
+                    "((]]]:-1",
+                    ":0",
+                    "[]:0",
+                    "():0",
+                    "[]():0",
+                    "[()]:0",
+                    "[][][][][][]:0",
+                    "()()()()()():0",
+                    "(]:1",
+                    "[):1",
+                    "([)]:2",
+            },
+            delimiter = ':'
+    )
+    void correct(String input, int expected) {
+        Assertions.assertEquals(expected, ex279.check(input));
+    }
+
+
 }
