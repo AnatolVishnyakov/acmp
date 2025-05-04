91cd4ba99b53eda3d495d0ece893e74b5b24bcdd
Anatol V. <malfenius@gmail.com>
2025-05-04 23:23:07 +0300

    Ex248. EuroEnglish

---
+++ src/test/java/algorithms/strings/Ex248Test.java
@@ -0,0 +1,26 @@
+package algorithms.strings;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ex248Test {
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+//                    "cacao and coffee:kakao and kofi"
+                    "ci:s",
+                    "ce:s",
+                    "ck:''",
+                    "success:suksess",
+                    "cck:kk",
+            },
+            delimiter = ':')
+    void correct(String input, String expected) {
+        Assertions.assertEquals(expected, Ex248.doEuroEnglish(input));
+    }
+}
